d1d5c487751da516a2162f23dad7613620c4489a
Munir Younosi <eaaamy@gmail.com>
2021-09-26 23:14:57 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb Project</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      {/* banner */}
+      <Banner />
+    </div>
+  )
+}
