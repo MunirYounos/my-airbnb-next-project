9baa80c1aa6b2e236f0c3e904130277530d11471
Munir Younosi <eaaamy@gmail.com>
2021-09-28 00:08:16 +0200

    frontpage elements done

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+
+function Footer() {
+	return (
+		<footer className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+			<div className="space-y-4 text-gray-800">
+				<h5 className="font-bold uppercase">About</h5>
+				<p>How Airbnb works</p>
+				<p>Newsroom</p>
+				<p>Investors</p>
+				<p>Airbnb Plus</p>
+				<p>Airbnb Luxe</p>
+			</div>
+			<div className="space-y-4 text-gray-800">
+				<h5 className="font-bold uppercase">Cummunity</h5>
+				<p>How Airbnb works</p>
+				<p>Newsroom</p>
+				<p>Ask for all relative informations</p>
+				<p>Airbnb Plus</p>
+				<p>Airbnb Luxe</p>
+			</div>
+			<div className="space-y-4 text-gray-800">
+				<h5 className="font-bold uppercase">Host</h5>
+				<p>How Airbnb works</p>
+				<p>Newsroom</p>
+				<p>Investors</p>
+				<p>Everything is accessible and easy to reach</p>
+				<p>Airbnb Luxe</p>
+			</div>
+			<div className="space-y-4 text-gray-800">
+				<h5 className="font-bold uppercase">Support</h5>
+				<p>How Airbnb works</p>
+				<p>Newsroom</p>
+				<p>Investors</p>
+				<p>Airbnb Plus</p>
+				<p>Airbnb Luxe</p>
+			</div>
+
+		</footer>
+	)
+}
+
+export default Footer
