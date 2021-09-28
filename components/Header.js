c9d5f1fc79ae4a6d4d9c1322e4db6e7071a40d1c
Munir Younosi <eaaamy@gmail.com>
2021-09-29 00:04:17 +0200

    calender added and 50 minuttes

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,29 @@
 import Image from 'next/image';
 import { SearchIcon, MenuIcon, UserCircleIcon, UsersIcon, GlobeAltIcon} from '@heroicons/react/solid';
+import { useState } from 'react';
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from 'react-date-range';
+
 function Header() {
+	const [searchInput, setSearchInput] = useState("");
+	const [startDate, setStartDate ] = useState(new Date());
+	const [endDate, setEndDate ] = useState(new Date());
+
+
+	const handleSelect = (ranges) =>{
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	}
+
+
+	const selectionRange = {
+		startDate: startDate,
+		endDate: endDate,
+		key: 'selection'
+	};
+
+
 	return (
 		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
 			<div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -11,7 +34,10 @@ function Header() {
 				/>
 			</div>
 			<div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-				<input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="start your search" />
+				<input 
+				value={searchInput}
+				onChange={(e) => setSearchInput(e.target.value)}
+				className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="start your search" />
 			<SearchIcon  className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
 			</div>
 			<div className="flex items-center justify-end space-x-4 text-gray-600">
@@ -22,6 +48,16 @@ function Header() {
 					<UserCircleIcon className="h-6" />
 				</div>
 			</div>
+			{searchInput && (
+				<div className="flex flex-col col-span-3 mx-auto mt-4">
+					<DateRangePicker 
+						ranges={[selectionRange]}
+						minDate={new Date()}
+						rangeColors={["#FD5b61"]}
+						onChange={handleSelect}
+					/>
+				</div>
+			)}
 		</header>
 	)
 }
